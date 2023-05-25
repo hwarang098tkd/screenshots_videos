58455765429ea9bda520ae8a0fc7d7dd7320f75c
David <hwa_rang098tkd@yahoo.gr>
2023-05-25 20:43:44 +0300

    οκλ

---
+++ load.py
@@ -0,0 +1,7 @@
+#import qtpy
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QInputDialog, QMenu, QAction, QTreeWidgetItem, QTreeWidgetItemIterator, QAbstractItemView, QHeaderView, QTableW
+
+
+# call the class for the main window
